e4fcfd6ae026b20e6b477d4c791af29f12121b93
Aleksey Mikaelyan <mikaelaleksey@gmail.com>
2020-06-29 19:39:24 +0300

    Создаем метод manage. Определяем непосредственные обязанности менеджера и переносим их в новый метод

--- src/main/java/org/example/shopproject/Manager.java
+++ src/main/java/org/example/shopproject/Manager.java
@@ -3,12 +3,15 @@ package org.example.shopproject;
 public class Manager {
 
     public void newWorkDay(Seller seller) {
+        System.out.println("Улучшает показатели продаж!");
+        System.out.println("Проводит обучение персонала!");
+        System.out.println("Начисляет зарплату!");
+    }
+
+    public void manage() {
         System.out.println("Проводит собрание!");
         System.out.println("Отвечает на вопросы!");
-        System.out.println("Улучшает показатели продаж!");
-        System.out.println("Составляет ведомость по закупкам товара!");
-        System.out.println("Проводит обучение персонала!");
         System.out.println("Просчитывает зарплату продавца!");
-        System.out.println("Начисляет зарплату!");
+        System.out.println("Составляет ведомость по закупкам товара!");
     }
 }
